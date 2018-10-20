db0423295dee7e98a3d30a68383415a69b5f3f33
Albert I <krascgq@outlook.co.id>
2018-10-20 22:23:20 +0800

    prima: Resolved indentation warnings detected by Linaro GCC 6.4.1
    * Using latest 2018.05 prebuilt release from releases.linaro.org.
    
    Change-Id: Ic952acd31549d8d2e197c50af7c0329fc22ee238
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- drivers/staging/prima/CORE/MAC/src/pe/lim/limUtils.c
+++ drivers/staging/prima/CORE/MAC/src/pe/lim/limUtils.c
@@ -4839,8 +4839,8 @@ tSirRetStatus
 limEnableHT20Protection(tpAniSirGlobal pMac, tANI_U8 enable,
     tANI_U8 overlap, tpUpdateBeaconParams pBeaconParams,tpPESession psessionEntry)
 {
-    if(!psessionEntry->htCapability)
-        return eSIR_SUCCESS; // this protection  is only for HT stations.
+        if(!psessionEntry->htCapability)
+            return eSIR_SUCCESS; // this protection  is only for HT stations.
 
         //overlapping protection configuration check.
         if(overlap)
@@ -5049,8 +5049,8 @@ tSirRetStatus
 limEnableHTNonGfProtection(tpAniSirGlobal pMac, tANI_U8 enable,
     tANI_U8 overlap, tpUpdateBeaconParams pBeaconParams,tpPESession psessionEntry)
 {
-    if(!psessionEntry->htCapability)
-        return eSIR_SUCCESS; // this protection  is only for HT stations.
+        if(!psessionEntry->htCapability)
+            return eSIR_SUCCESS; // this protection  is only for HT stations.
 
         //overlapping protection configuration check.
         if(overlap)
@@ -5120,8 +5120,8 @@ tSirRetStatus
 limEnableHTLsigTxopProtection(tpAniSirGlobal pMac, tANI_U8 enable,
     tANI_U8 overlap, tpUpdateBeaconParams pBeaconParams,tpPESession psessionEntry)
 {
-    if(!psessionEntry->htCapability)
-        return eSIR_SUCCESS; // this protection  is only for HT stations.
+        if(!psessionEntry->htCapability)
+            return eSIR_SUCCESS; // this protection  is only for HT stations.
 
         //overlapping protection configuration check.
         if(overlap)
@@ -5193,8 +5193,8 @@ tSirRetStatus
 limEnableHtRifsProtection(tpAniSirGlobal pMac, tANI_U8 enable,
     tANI_U8 overlap, tpUpdateBeaconParams pBeaconParams,tpPESession psessionEntry)
 {
-    if(!psessionEntry->htCapability)
-        return eSIR_SUCCESS; // this protection  is only for HT stations.
+        if(!psessionEntry->htCapability)
+            return eSIR_SUCCESS; // this protection  is only for HT stations.
 
 
         //overlapping protection configuration check.
